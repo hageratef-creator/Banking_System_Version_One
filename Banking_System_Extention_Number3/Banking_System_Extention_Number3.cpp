9ac30f20eff35b0065e04a93221409e397da6346
hageratef <hageratef217@gmail.com>
2026-02-13 03:26:39 +0200

    Adding OOP Version Of The Project And Adding More Features

---
+++ Banking_System_Extention_Number3/Banking_System_Extention_Number3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+//#include "clsLoginScreen.h"
+
+using namespace std;
+
+
+
+int main()
+{
+
+  //  clsLoginScreen::ShowLoginScreen();
+
+    system("pause>0");
+    return 0;
+
+
+}
+
+
+
+
+
+
+
